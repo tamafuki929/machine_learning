80dcb89d79b805917629be08971bcfe159de2e38
tamafuki929 <tamafuki929@gmail.com>
2022-12-04 21:51:08 +0900

    add preprocess

--- src/tabular_analysis/preprocess.py
+++ src/tabular_analysis/preprocess.py
@@ -1,6 +1,29 @@
+# %% import general libraries
+import numpy as np
+import pandas as pd
+
+# %% import libraries for preprocessing
+from sklearn.impute import SimpleImputer, MissingIndicator
+from sklearn.pipeline import FeatureUnion
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 
 # %% drop na and use only numerical columns
 def make_baseline_dataset(data):
     data = data.select_dtypes(include = np.number)
     data.dropna(axis = 1)
     return data
+
+# %% make pipleline to impute nan and create nan or not column
+base_trans = FeatureUnion[
+            ("simpleimputer", SimpleImputer(strategy = "median")), 
+            ("missingindicator", MissingIndicator())
+            ]
+ct = ColumnTransformer(
+        transfomers = [
+            "fillna_and_missindicator", base_trans, ["xxx", "yyy"], 
+            "onehotencode", OneHotEncoder(), "xxxx"
+        ]
+    )
+
+ct.fit_transform(data)
--- src/tabular_analysis/readme.md
+++ src/tabular_analysis/readme.md
@@ -1,14 +1,22 @@
 # Tabular Data Analysis
 
+## Preprocessing
+* sklearnを使用した欠損値補完についてはサイト[^4]を参照  
+* pipelineの活用に関してはサイト[^5]を参照
+
 ## GBDT 
 
 ### LightGBM
-LightGBMとXGBの比較についてはnotebook[^2]を参照. 　
-LightGBMのtuningについてはnotebook[^3]を参照.  
-LightGBMのcvの仕様は文献[^1]にあるように, 特徴的なものとなっている.  
+* LightGBMとXGBの比較についてはnotebook[^2]を参照
+* LightGBMのtuningについてはnotebook[^3]を参照
+* LightGBMのcvの仕様は文献[^1]にあるように, 特徴的なものとなっている
 
 [^1]: https://blog.amedama.jp/entry/lightgbm-cv-implementation
 
 [^2]: https://www.kaggle.com/code/bextuychiev/how-to-beat-the-heck-out-of-xgboost-with-lightgbm/notebook
 
 [^3]: https://www.kaggle.com/code/bextuychiev/lgbm-optuna-hyperparameter-tuning-w-understanding
+
+[^4]: https://qiita.com/FukuharaYohei/items/9830d5760595619352a5
+
+[^5]: https://dev.classmethod.jp/articles/concatenate_result_with_featureunion/
