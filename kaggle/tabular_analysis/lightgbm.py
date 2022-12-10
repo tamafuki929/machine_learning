ddc666bf08d22249b9a39ba4f84c9413082b0474
tamafuki929 <tamafuki929@gmail.com>
2022-12-10 16:59:40 +0900

    modify lgbcls

--- kaggle/tabular_analysis/lightgbm.py
+++ kaggle/tabular_analysis/lightgbm.py
@@ -1,11 +1,10 @@
 # %% 
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 
-from sklearn.model_selection import cross_val_predict, KFold, StratifiedKFold
+from sklearn.model_selection import cross_val_predict, KFold, StratifiedKFold, train_test_split
 from sklearn.metrics import roc_auc_score, r2_score
-
+from tqdm import tqdm
 import lightgbm as lgb
 import optuna
 
@@ -76,8 +75,48 @@ def lgb_cv_lgvreg(X, y, params = {}, random_state = 42):
     return cv_result
 
 
+def random_holdout_lgbcls(X, y, cv, params = {}, n_iter = 10, random_state = 42):
+    _params = {
+        "objective"    : "binary", # or multiclass, 
+        # "num_class"  : 3, 
+        "boosting_type": "gbdt", 
+        "metrics"      : "binary_logloss", # multi_logloss
+        "random_state" : random_state, 
+        "verbose"      : -1
+    }
+    _params.update(params)
+    
+    scores = []
+    models = []
+    for i in range(n_iter):
+        X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size = 0.1, 
+                                                              stratify = y, 
+                                                              random_state = 42 + i)
+    
+        dtrain = lgb.Dataset(X_train, label = y_train)
+        dvalid = lgb.Dataset(X_valid, label = y_valid, free_raw_data = False)
+        model = lgb.train(
+                    _params, dtrain, 
+                    valid_sets = [dtrain, dvalid], 
+                    num_boost_round = 1000, 
+                    callbacks=[
+                        lgb.early_stopping(stopping_rounds=10, verbose=True), 
+                        lgb.log_evaluation(period=100)
+                    ]
+            )
+        y_pred = model.predict(dvalid.get_data())
+        auc_score = roc_auc_score(dvalid.get_label(), y_pred)
+        
+        scores.append(auc_score)
+        models.append(model)
+    
+    print(f"validation result: {np.mean(scores)}")
+    return scores, models
+
+
+
 # %% baseline to use sllearn or manual cv
-def manual_cv_lgbcls(X, y, cv, params = {}, random_state = 42):
+def manual_cv_lgbcls(X, y, cv, data_transformer, params = {}, random_state = 42):
     _params = {
         "objective"    : "binary", # or multiclass, 
         # "num_class"  : 3, 
@@ -91,10 +130,14 @@ def manual_cv_lgbcls(X, y, cv, params = {}, random_state = 42):
     scores = []
     models = []
     for _, (train_id, val_id) in tqdm(enumerate(cv.split(X, y))):
-        dtrain = lgb.Dataset(X[train_id], label = y[train_id])
-        dvalid = lgb.Dataset(X[val_id]  , label = y[val_id], free_raw_data = False)
+        if data_transformer:
+            processed_X, processed_y = data_transformer.fit_transform(X, y, X.iloc[train_id].index)
+        else:
+            processed_X, processed_y = X, y
+        dtrain = lgb.Dataset(processed_X.iloc[train_id], label = processed_y.iloc[train_id])
+        dvalid = lgb.Dataset(processed_X.iloc[val_id]  , label = processed_y.iloc[val_id], free_raw_data = False)
         
-        # if there is no improvement in dtrain or dvalid, stop learning
+        # if there is no improvement in dtrain or dvalid, execute early stopping
         model = lgb.train(
                 _params, dtrain, 
                 valid_sets = [dtrain, dvalid], 
@@ -143,32 +186,42 @@ def lgb_cv_lgbcls(X, y, cv, params = {}, random_state = 42):
     print(f'logloss mean = {cv_result["binary_logloss-mean"][-1]}')
     print(f'auc mean = {cv_result["auc-mean"][-1]}')
     return cv_result
+
+
+# get feature importance from multiple models
+def models_feature_importance(models, columns, importance_type = "gain"):
+    fi_array = np.zeros(len(columns))
+    for model in models:
+        fi_array += model.feature_importance(importance_type=importance_type)
+    fi_df = pd.DataFrame(fi_array / len(models), 
+                         index = columns, columns=['importance'])
+    return fi_df.sort_values(by = "importance", ascending = False)
     
 # hyperparameter tuning in lightGBM
 def tuning_lightgbm(X, y, random_state = 42):
+    cv = KFold(n_splits = 5, shuffle = True, random_state = 42)
     
     def objective(trial, X, y):
         params = {
-            "n_estimators" : trial.suggest_categorical("n_estimators", [2000]),
+            "num_boost_round" : trial.suggest_categorical("num_boost_round", [2000]),
             "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.3),
-            "num_leaves": trial.suggest_int("num_leaves", 20, 3000, step=20), 
-            "min_data_in_leaf": trial.suggest_int("min_data_in_leaf", 20, 1020, step=100), 
+            "num_leaves": trial.suggest_int("num_leaves", 20, 1000, step=20), 
+            "min_data_in_leaf": trial.suggest_int("min_data_in_leaf", 20, 1000, step=100), 
             "lambda_l1": trial.suggest_int("lambda_l1", 0, 100, step=5), 
             "lambda_l2": trial.suggest_int("lambda_l2", 0, 100, step=5), 
             "min_gain_to_split": trial.suggest_float("min_gain_to_split", 0, 15), 
-            "bagging_freq": trial.suggest_float("bagging_freq", 0.5, 1.0, step = 0.1),
-            "feature_fraction": trial.suggest_float("feature_fraction", 0.2, 0.95, step=0.1),
+            "bagging_fraction": trial.suggest_float("bagging_fraction", 0.5, 1.0, step = 0.1),
+            "feature_fraction": trial.suggest_float("feature_fraction", 0.2, 0.95, step=0.1)
         }
-        cv = KFold(n_splits = 5, shuffle = True, random_state = 42)
-        scores, models = manual_cv_lgbcls(X, y, cv = cv)
+        scores, models = manual_cv_lgbcls(X, y, cv = cv, params = params)
         return np.mean(scores)
     
     study = optuna.create_study(
-            direction = "minimize", # or ma 
+            direction = "maximize", # or minimize
             sampler   = optuna.samplers.TPESampler(seed = random_state), 
             study_name = "lgb classifier")
     func  = lambda trial: objective(trial, X, y)
-    study.optimize(func, n_trials=50)
+    study.optimize(func, n_trials=500)
     
     return study
 # %%
