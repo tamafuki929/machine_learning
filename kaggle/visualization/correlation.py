4905151e34f67ea1fe9d0a255f7325eed80b51ee
tamafuki929 <tamafuki929@gmail.com>
2022-12-18 17:46:44 +0900

    add mutual info

--- kaggle/visualization/correlation.py
+++ kaggle/visualization/correlation.py
@@ -7,6 +7,7 @@ import sklearn
 
 import scipy.stats as st
 import itertools
+from sklearn.feature_selection import mutual_info_classif, mutual_info_regression
 
 # %% plot correlation
 def corr(data):
@@ -53,3 +54,45 @@ def corr_cat_vs_con(data, cat_cols, con_cols):
             corr[cat_i, con_i] = r
     corr_df = pd.DataFrame(corr, index = cat_cols, columns = con_cols)
     return corr_df
+
+
+def corr_mutual_matrix(df, random_state = 42):
+    con_cols = df.select_dtypes(include = np.number).columns.to_list()
+    cat_cols = df.select_dtypes(exclude = np.number).columns.to_list()
+    columns = con_cols + cat_cols
+    corr = pd.DataFrame(np.identity(len(columns)), index = columns, columns = columns)
+    for col in columns:
+        target_df = df[col].dropna()
+        if target_df.dtype == np.number:
+            mi = mutual_info_regression(target_df.values.reshape(-1, 1), 
+                                        target_df.values, random_state=random_state)
+        else:
+            mi = mutual_info_classif(target_df.astype(np.number).values.reshape(-1, 1), 
+                                     target_df.values, random_state = random_state)
+        corr.loc[col, col] = mi
+    for cols in itertools.combinations(columns, 2):
+        target_df = df[list(cols)].dropna()
+        if target_df.iloc[:, 1].dtype == np.number:
+            mi = mutual_info_regression(target_df[cols[0]].values.reshape(-1, 1), 
+                                        target_df[cols[1]].values, 
+                                        random_state=random_state)
+        else:
+            mi = mutual_info_classif(target_df[cols[0]].astype(np.number).values.reshape(-1, 1), 
+                                     target_df[cols[1]].values, 
+                                     random_state = random_state)
+            corr.loc[cols[0], cols[1]] = 2 * mi
+            corr.loc[cols[0], cols[1]] /= corr.loc[cols[0], cols[0]] + corr.loc[cols[1], cols[1]]
+    corr += corr.T - np.diag(np.diag(corr))
+    for col in columns:
+        corr.loc[col, col] = 1.
+    return pd.DataFrame(corr, index = columns, columns = columns)
+
+def feature_selection_mutual_info(df, th = 0.9, random_state = 42):
+    while True:
+        mutual_list = corr_mutual_matrix(df, random_state = random_state).sum(axis = 1) - 1.
+        max_mi, max_mi_id = mutual_list.max(), mutual_list.idxmax()
+        if max_mi > th: 
+            df = df.drop(max_mi_id, axis = 1)
+        else:
+            break
+    return corr_mutual_matrix(df, random_state = random_state)
